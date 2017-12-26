b5782bfa524162538ad3a36eefe32e37cf9ebc69
Brock McElroy <28877984+brxck@users.noreply.github.com>
2017-12-26 14:27:31 -0700

    Initial commit.

---
+++ script/rest_requestor.rb
@@ -0,0 +1,7 @@
+require "rest-client"
+url = "http://localhost:3000/users"
+
+puts RestClient.get(url)
+puts RestClient.get(url + "/show/1")
+puts RestClient.get(url + "/new")
+puts RestClient.get(url + "/edit")
